c1ebe44dfd62fc419b0e103c5ec1ecc287f277e6
Yoga Kanneboina <yogapriyak7@gmail.com>
2018-03-22 20:12:47 +0000

    My first commit

---
+++ bootstrap-3.3.7-dist/js/my.js
@@ -0,0 +1,7 @@
+// JavaScript File
+function openNav(){
+  document.getElementById("side").style.display = "block";
+}
+function closeNav(){
+  document.getElementById("side").style.display = "none";
+}
